f42cc007fe97eb782389f158746c69ba5844abef
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-07 17:06:51 -0400

    basic operator practice

---
+++ coding practices/4. Basic Operators/script.js
@@ -0,0 +1,33 @@
+const country = "Canada";
+const continent = "North America";
+let population = 10000;
+const isIsland = false;
+let language;
+language = "English";
+
+let curPopulation = population / 2;
+curPopulation++;
+console.log(curPopulation);
+
+if (population > 6000000) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+
+if (population > 33000000) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+
+const description =
+    country +
+    " is in " +
+    continent +
+    ", and its " +
+    population +
+    " people speaks " +
+    language;
+
+console.log(description);
