7e1718ac241d1b98439b55fcdda4f9198c449923
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-07 15:32:22 -0400

    what is javascript

---
+++ Notes.js
@@ -0,0 +1,19 @@
+/*
+Javascript is a 
+    high-level: don't need to worry about complex things like memory management
+    object-oriented: based on objects, which stores most kind of data
+    multi-paradigm: we can use different kind of programming styles
+    programming language: instruct the computer to do things
+
+
+
+
+*/
+
+// output with pop up window
+alert("Hello World");
+
+let js = "amazing";
+if (js === "amazing") alert("Javascript is FUN!");
+
+js = "boring";
