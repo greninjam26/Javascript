f05e73b634297696ba077a3ab62263c639ba3b1a
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-07 16:00:36 -0400

    data types

--- Notes.js
+++ Notes.js
@@ -10,10 +10,31 @@ Javascript is a
 
 */
 
+// VALUES
+// primitive
+let js = "amazing";
+// 7 data types (JAVASCRIPT AUTOMATICLY DETERMINES THE DATA TYPE)
+// Javascript have dynamic typing, which means we can change the data type of variables
+let number = 23; //decimals and integers
+let string = "Greninja"; //text
+let boolean = true; //true and false
+let undefine; // no value
+null; //empty value
+// symbol: value that is unique and can't be changed
+// BigInt: numbers that are too large for the number type
+
+// object
+let me = {
+    name: "Greninja",
+};
+
+// output the type of the value
+// typeof have the bug that returns "null" as object
+console.log(typeof js);
+
 // output with pop up window
 alert("Hello World");
 
-let js = "amazing";
 if (js === "amazing") alert("Javascript is FUN!");
 
 js = "boring";
